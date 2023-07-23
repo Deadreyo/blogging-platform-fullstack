daa178f285f8c41ed2f1e98e391cc3c64531d72d
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2023-07-23 17:58:12 +0300

    feat: load env file

--- backend-nestjs/src/app.module.ts
+++ backend-nestjs/src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogsModule } from './blogs/blogs.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { env } from 'process';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [BlogsModule],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(env.MONGO_URL), BlogsModule],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
