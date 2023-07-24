441df757e8de91c5c5825ea168e43f32ccb0f943
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2023-07-24 11:30:58 +0300

    refractor: remove unneeded files

--- backend-nestjs/src/app.module.ts
+++ backend-nestjs/src/app.module.ts
@@ -4,10 +4,23 @@ import { AppService } from './app.service';
 import { BlogsModule } from './blogs/blogs.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { env } from 'process';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(env.MONGO_URL), BlogsModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), MongooseModule.forRoot(env.MONGO_URL), BlogsModule, UsersModule, AuthModule ,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('JWT_SECRET'),
+          signOptions: { expiresIn: '1d' },
+        };
+      },
+      inject: [ConfigService],
+      global: true,
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
