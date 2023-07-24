8db48f819c696c7fd8dd252e49b8abc734e251b1
Ahmed Atwa <Ahmedatwa866@yahoo.com>
2023-07-24 11:30:39 +0300

    feat: users module

---
+++ backend-nestjs/src/users/users.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+
+
+@Injectable()
+export class UsersService {
+
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  create(createUserDto: CreateUserDto) {
+    const createdUser = new this.userModel(createUserDto);
+    return createdUser.save();
+  }
+
+  async findOne(email: string) {
+    return await this.userModel.findOne({ email }).exec();
+  }
+}
